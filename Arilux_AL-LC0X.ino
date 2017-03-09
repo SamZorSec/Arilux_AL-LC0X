efa0ba2ee9c24a561690e2297a821eb7f9885209
Robbie Trencheny <me@robbiet.us>
2017-03-08 22:47:57 -0800

    Add fade/transition time support

--- Arilux_AL-LC0X.ino
+++ Arilux_AL-LC0X.ino
@@ -87,6 +87,21 @@ WiFiClient        wifiClient;
 #endif
 PubSubClient        mqttClient(wifiClient);
 
+// Real values to write to the LEDs (ex. including brightness and state)
+byte realRed = 0;
+byte realGreen = 0;
+byte realBlue = 0;
+
+// Globals for fade/transitions
+bool startFade = false;
+unsigned long lastLoop = 0;
+int transitionTime = 0;
+bool inFade = false;
+int loopCount = 0;
+int stepR, stepG, stepB;
+int redVal, grnVal, bluVal;
+
+
 ///////////////////////////////////////////////////////////////////////////
 //  SSL/TLS
 ///////////////////////////////////////////////////////////////////////////
@@ -143,12 +158,24 @@ void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
     int brightness = root["brightness"];
     int transition = root["transition"];
     int white_value = root["white_value"];
+    if (root.containsKey("transition")) {
+      transitionTime = root["transition"];
+    } else {
+      transitionTime = 0;
+    }
 
     int red_color = root["color"]["r"];
     int green_color = root["color"]["g"];
     int blue_color = root["color"]["b"];
 
+    realRed = red_color;
+    realGreen = green_color;
+    realBlue = blue_color;
+
     bool boolState = state.equals("ON");
+
+    startFade = true;
+    inFade = false; // Kill the current fade
 
     if (arilux.getState() != boolState) {
       arilux.setState(boolState);
@@ -244,7 +271,6 @@ void connectMQTT(void) {
           root.printTo(configBuf, sizeof(configBuf));
           publishToMQTT(HOME_ASSISTANT_MQTT_DISCOVERY_TOPIC, configBuf);
         #endif
-        flash(true);
       } else {
         DEBUG_PRINTLN(F("ERROR: The connection to the MQTT broker failed"));
         DEBUG_PRINT(F("Username: "));
@@ -253,7 +279,6 @@ void connectMQTT(void) {
         DEBUG_PRINTLN(MQTT_PASS);
         DEBUG_PRINT(F("Broker: "));
         DEBUG_PRINTLN(MQTT_SERVER);
-        flash(false);
       }
 
       subscribeToMQTTTopic(ARILUX_MQTT_STATE_COMMAND_TOPIC);
@@ -710,7 +735,6 @@ void setup() {
   ArduinoOTA.setHostname(MQTT_CLIENT_ID);
   ArduinoOTA.onStart([]() {
     DEBUG_PRINTLN("OTA Beginning!");
-    flash(true);
   });
   ArduinoOTA.onError([](ota_error_t error) {
     DEBUG_PRINT("ArduinoOTA Error[");
@@ -746,6 +770,7 @@ void loop() {
   // Handle commands
   handleCMD();
   yield();
+  handleFade();
   connectMQTT();
   mqttClient.loop();
   yield();
@@ -756,38 +781,113 @@ void loop() {
 ///////////////////////////////////////////////////////////////////////////
 //  Utilities
 ///////////////////////////////////////////////////////////////////////////
-/*
-   Helper function to show success/failure of a task with the light strip.
-*/
 
-void flash(bool success) {
-  if (success) {
-    arilux.setAll(0, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 255, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 255, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 255, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 0, 0, 0, 0);
-  } else {
-    arilux.setAll(0, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(255, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(255, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(255, 0, 0, 0, 0);
-    delay(300);
-    arilux.setAll(0, 0, 0, 0, 0);
-  }
+void handleFade(void) {
+  if (startFade) {
+      // If we don't want to fade, skip it.
+      if (transitionTime == 0) {
+        arilux.setColor(realRed, realGreen, realBlue);
+
+        redVal = realRed;
+        grnVal = realGreen;
+        bluVal = realBlue;
+
+        startFade = false;
+        publishColorChange();
+      } else {
+        loopCount = 0;
+        stepR = calculateStep(redVal, realRed);
+        stepG = calculateStep(grnVal, realGreen);
+        stepB = calculateStep(bluVal, realBlue);
+
+        inFade = true;
+      }
+    }
+
+    if (inFade) {
+      startFade = false;
+      unsigned long now = millis();
+      if (now - lastLoop > transitionTime) {
+        if (loopCount <= 1020) {
+          lastLoop = now;
+
+          redVal = calculateVal(stepR, redVal, loopCount);
+          grnVal = calculateVal(stepG, grnVal, loopCount);
+          bluVal = calculateVal(stepB, bluVal, loopCount);
+
+          arilux.setColor(redVal, grnVal, bluVal); // Write current values to LED pins
+
+          DEBUG_PRINT("Fade Loop count: ");
+          DEBUG_PRINTLN(loopCount);
+          loopCount++;
+        } else {
+          inFade = false;
+          publishColorChange();
+        }
+      }
+    }
+}
+
+// From https://www.arduino.cc/en/Tutorial/ColorCrossfader
+/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
+*
+* The program works like this:
+* Imagine a crossfade that moves the red LED from 0-10,
+*   the green from 0-5, and the blue from 10 to 7, in
+*   ten steps.
+*   We'd want to count the 10 steps and increase or
+*   decrease color values in evenly stepped increments.
+*   Imagine a + indicates raising a value by 1, and a -
+*   equals lowering it. Our 10 step fade would look like:
+*
+*   1 2 3 4 5 6 7 8 9 10
+* R + + + + + + + + + +
+* G   +   +   +   +   +
+* B     -     -     -
+*
+* The red rises from 0 to 10 in ten steps, the green from
+* 0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
+*
+* In the real program, the color percentages are converted to
+* 0-255 values, and there are 1020 steps (255*4).
+*
+* To figure out how big a step there should be between one up- or
+* down-tick of one of the LED values, we call calculateStep(),
+* which calculates the absolute gap between the start and end values,
+* and then divides that gap by 1020 to determine the size of the step
+* between adjustments in the value.
+*/
+int calculateStep(int prevValue, int endValue) {
+    int step = endValue - prevValue; // What's the overall gap?
+    if (step) {                      // If its non-zero,
+        step = 1020/step;            //   divide by 1020
+    }
+
+    return step;
+}
+
+/* The next function is calculateVal. When the loop value, i,
+*  reaches the step size appropriate for one of the
+*  colors, it increases or decreases the value of that color by 1.
+*  (R, G, and B are each calculated separately.)
+*/
+int calculateVal(int step, int val, int i) {
+    if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+        if (step > 0) {              //   increment the value if step is positive...
+            val += 1;
+        }
+        else if (step < 0) {         //   ...or decrement it if step is negative
+            val -= 1;
+        }
+    }
+
+    // Defensive driving: make sure val stays in the range 0-255
+    if (val > 255) {
+        val = 255;
+    }
+    else if (val < 0) {
+        val = 0;
+    }
+
+    return val;
 }
