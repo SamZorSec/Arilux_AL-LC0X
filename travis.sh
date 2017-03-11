2617cdd3cee7b980ab96db77b9449c1076c3fb1c
Robbie Trencheny <me@robbiet.us>
2017-03-11 14:53:54 -0800

    Setup defines properly

--- travis.sh
+++ travis.sh
@@ -1,27 +1,47 @@
-# verify the ino, and save stdout & stderr to a variable
-# we have to avoid reading the exit code of local:
-# "when declaring a local variable in a function, the local acts as a command in its own right"
-local build_stdout
-build_stdout=$(arduino -v --verbose-build --verify --board esp8266:esp8266:generic Arilux_AL-LC0X.ino 2>&1)
+function build() {
 
-# echo output if the build failed
-if [ $? -ne 0 ]; then
+  if [ -z "$TYPE" ]; then
+    echo -e "#define $TYPE\n$(cat config.example.h)" > config.h
+  fi
 
-  # heavy X
-  echo -e "\xe2\x9c\x96"
+  if [ -z "$REMOTE" ]; then
+    echo -e "#define $REMOTE\n$(cat config.example.h)" > config.h
+  fi
 
-  echo -e "----------------------------- DEBUG OUTPUT -----------------------------\n"
-  echo "$build_stdout"
-  echo -e "\n------------------------------------------------------------------------\n"
+  if [ -z "$MQTT_MODE" ]; then
+    echo -e "#define $MQTT_MODE\n$(cat config.example.h)" > config.h
+  fi
 
-  # mark as fail
-  exit_code=1
+  if [ -z "$MQTT_DISCOVERY" ]; then
+    echo -e "#define $MQTT_DISCOVERY\n$(cat config.example.h)" > config.h
+  fi
 
-else
+  # verify the ino, and save stdout & stderr to a variable
+  # we have to avoid reading the exit code of local:
+  # "when declaring a local variable in a function, the local acts as a command in its own right"
+  local build_stdout
+  build_stdout=$(arduino -v --verbose-build --verify --board esp8266:esp8266:generic Arilux_AL-LC0X.ino 2>&1)
 
-  # heavy checkmark
-  echo -e "\xe2\x9c\x93"
+  # echo output if the build failed
+  if [ $? -ne 0 ]; then
 
-fi
+    # heavy X
+    echo -e "\xe2\x9c\x96"
 
-return $exit_code
+    echo -e "----------------------------- DEBUG OUTPUT -----------------------------\n"
+    echo "$build_stdout"
+    echo -e "\n------------------------------------------------------------------------\n"
+
+    # mark as fail
+    exit_code=1
+
+  else
+
+    # heavy checkmark
+    echo -e "\xe2\x9c\x93"
+
+  fi
+
+  return $exit_code
+
+}
