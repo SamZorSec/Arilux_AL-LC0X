d463b51ba6b89bb5de977e6f78919728ab11086b
Robbie Trencheny <me@robbiet.us>
2019-03-16 01:24:51 -0700

    Improve README

--- config.example.h
+++ config.example.h
@@ -75,8 +75,8 @@
 #define HOME_ASSISTANT_MQTT_DISCOVERY_PREFIX   "homeassistant"
 
 // Enable Home Assistant JSON Attributes support. Requires ArduinoJSON library to be installed.
-#define HOME_ASSISTANT_MQTT_ATTRIBUTES
-#define HOME_ASSISTANT_MQTT_ATTRIBUTES_TOPIC_TEMPLATE   "%s/attributes"
+// #define HOME_ASSISTANT_MQTT_ATTRIBUTES
+// #define HOME_ASSISTANT_MQTT_ATTRIBUTES_TOPIC_TEMPLATE   "%s/attributes"
 
 // Enable JSON. Requires ArduinoJSON library to be installed.
 // Home Assistant supports more features such as transitions, effects and flashing via JSON only.
